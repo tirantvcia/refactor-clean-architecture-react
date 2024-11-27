0848611bcaa1fee525995465a1577c2c19a4f373
tirantvcia <tirantvcia@gmail.com>
2024-11-27 18:09:36 +0100

    Refactor, move test into products-page folder

--- src/pages/_test_/products-page/ProductsPage.spec.tsx
+++ src/pages/_test_/products-page/ProductsPage.spec.tsx
@@ -1,9 +1,9 @@
 import { afterAll, afterEach, beforeAll, describe, test } from "vitest";
 import { render, RenderResult, screen } from "@testing-library/react";
 import { ReactNode } from "react";
-import { AppProvider } from "../../context/AppProvider";
-import { ProductsPage } from "../ProductsPage";
-import { MockWebServer } from "../../tests/MockWebServer";
+import { AppProvider } from "../../../context/AppProvider";
+import { ProductsPage } from "../../ProductsPage";
+import { MockWebServer } from "../../../tests/MockWebServer";
 import productsReponse from "./data/productsResponse.json"
 
 const mockWebServer = new MockWebServer();
